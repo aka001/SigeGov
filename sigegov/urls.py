09c7d66b3f87140ccd36543c6ffdc0db322c4de0
Akash Agrawall <akash.wanted@gmail.com>
2015-04-20 23:58:55 +0530

    Removed an error

--- sigegov/urls.py
+++ sigegov/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),
     # ex: /polls/5/
     
-    url(r'^enter_data/$',views.enter_data,name='enter_dat),
+    url(r'^enter_data/$',views.enter_data,name='enter_data'),
     url(r'^view_request_thanks/(?P<requestID>\S+)/$',views.view_request_thanks,name='view_request_thanks'),
     url(r'^view_request/(?P<requestID>\S+)/$',views.view_request,name='view_request'),
     url(r'^requestblood_form/$',views.requestblood_form,name='requestblood_form'),
